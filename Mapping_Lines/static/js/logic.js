89802efa2382758ef00f2e29327e90bac484a401
Sarah Gallagher <sarahrosegallagher@gmail.com>
2022-05-16 13:16:59 -0700

    mapping lines practice

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,5 +1,12 @@
+// NTS: RESUME AT SKILL DRILL 13.4.3
 
-// boilerplate code
+//  mapbox ids:
+    // mapbox/streets-v11
+    // mapbox/outdoors-v11
+    // mapbox/light-v10
+    // mapbox/dark-v10
+    // mapbox/satellite-v9
+    // mapbox/satellite-streets-v11
 
 // console log test
 console.log("working");
@@ -9,47 +16,33 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
     center: [
-      39.0, -98.0
-    ],
+      37.6213, -95
+        ],
     zoom: 5
   });
 
-//13.4.2
-  // add multiple markers 
-  // loop thru array 
+// Coordinates for each point to be used in the line.
+let line = [
+  [33.9416, -118.4085],
+  [30.283910,-97.704840],
+  [43.680706,-79.6115899],
+  [40.64357,-73.78203]
+];
 
-//get data for array
-  //need script src in index.html 
-let cityData = cities;
-
-//loop, use L.marker(), .bindPopup() method [toLocaleString() method for population format], .addTo(map)
-
-// cityData.forEach(function(city){
-//   console.log(city)
-//   L.marker(city.location)
-//   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3> Population " + city.population.toLocaleString() + "</h3>")
-//   .addTo(map);
-// });
-
-// same as above w circle based on pop
-cityData.forEach(function(city){
-  console.log(city)
-  L.circleMarker(city.location, {
-    radius: city.population/200000,
-    color: "orange",
-    fillColor: "#EE8E1B",
-    lineWeight: 4
-  })
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3> Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "blue",
+  dashArray: "10, 10",
+  opacity: "0.5",
+  weight: "4"
+}).addTo(map);
 
 // tile layer  https://leafletjs.com/examples/quick-start/
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/outdoors-v11',
+    id: 'mapbox/light-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
