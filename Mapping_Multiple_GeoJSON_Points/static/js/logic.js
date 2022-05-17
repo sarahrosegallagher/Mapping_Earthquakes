3f17459e0d627681ab8d10b749e1c521dd811f32
Sarah Gallagher <sarahrosegallagher@gmail.com>
2022-05-16 21:47:45 -0700

    multiple geojson point file structure

---
+++ Mapping_Multiple_GeoJSON_Points/static/js/logic.js
@@ -0,0 +1,80 @@
+// intellisense path
+/// <reference path = "../../typings/index.d.ts" />
+
+//  mapbox ids:
+    // mapbox/streets-v11
+    // mapbox/outdoors-v11
+    // mapbox/light-v10
+    // mapbox/dark-v10
+    // mapbox/satellite-v9
+    // mapbox/satellite-streets-v11
+    //  mapbox/navigation-preview-night-v2
+
+// console log test
+console.log("working");
+
+
+// map method 2
+// Create the map object with a center and zoom level.
+let map = L.map("mapid", {
+    center: [
+      37.6, -122.4
+        ],
+    zoom: 12
+  });
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// call back fxn: on each feature
+L.geoJSON(sanFranAirport, {
+  onEachFeature: function(feature, layer){
+    console.log(layer);
+    layer.bindPopup("<h2>Airport Code: "+feature.properties.faa +"</h2> <hr> <h3>Airport Name: " + feature.properties.name + "</h3>");
+  }
+}).addTo(map);
+
+// call back fxn: point to layer
+// L.geoJSON(sanFranAirport,{
+//   // pointToLayer method to add each feature into marker
+//   // chain bindPopup()
+//   pointToLayer: function(feature, latlng){
+//     // log for testing 
+//     console.log(feature);
+//     return L.marker(latlng)
+//     .bindPopup("<h2>"+feature.properties.city +"</h2>");
+//   }
+// }).addTo(map);
+
+// tile layer  https://leafletjs.com/examples/quick-start/
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/navigation-preview-night-v2/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/navigation-preview-night-v2',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+//then add tile layer  
+streets.addTo(map);
